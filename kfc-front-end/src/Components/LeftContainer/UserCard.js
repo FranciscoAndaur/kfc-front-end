b1df47870e15ded03ee2b19535c949a2f61d5d69
Fraciscv <Francisco.Andaur@icloud.com>
2020-09-09 10:01:25 -0400

    User Card / Cards

--- kfc-front-end/src/Components/LeftContainer/UserCard.js
+++ kfc-front-end/src/Components/LeftContainer/UserCard.js
@@ -7,14 +7,14 @@ class UserCard extends React.Component {
     //todo need help rendering USERS TO PAGE
     
     render() {
-        console.log(this.props.users.map(users => console.log("from map in Usercard", users)))
-        let allUsers = this.props.users.map(users => {
+        console.log(this.props.users.forEach(users => console.log("from map in Usercard", users)))
+        let allUsers = this.props.users.forEach(users => {
             // return <Card userObj={users} key={users.id} />
             return <>
-            <div className="uk-light uk-background-secondary uk-padding">
-                        <h3>{console.log(users.name)}</h3>
+            <div key={users.id}className="uk-light uk-background-secondary uk-padding">
+                        <h3>{users.name}</h3>
                         <p>user snippet has to be this long </p>
-                        <button className="uk-button uk-button-default">Button</button>
+                        <button className="uk-button uk-button-default">button</button>
                     </div>
 
             </>
